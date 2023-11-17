80d024c09d555c6c262a5901f021f5b93e57017a
Keiichi Tsuda <keiichi.tsuda@gmail.com>
2023-11-18 08:34:10 +0900

    minor fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <main className="App-contents">
         <SimpleForm setGameRecord={setGameRecord} />
         <br/>
-        User Name: {gameRecord[0]?.ReplayReduced.fighter_id || 'XXXXXXXX'}
+        User Name: {gameRecord[0]?.ReplayReduced.fighter_id || 'XXXXXXXX'} （{gameRecord[0]?.CharacterName || 'XXXX'}）
         <ul>
           <li>１週間の勝率（ランクマッチのみ）</li>
             <SimpleTable gameRecord={gameRecord} />
@@ -28,7 +28,7 @@ function App() {
         *** 使い方 ***<br/>
         <ul>
           <li>User Codeを指定してSubmitボタンを押すと過去１週間分の対戦情報に基づく集計結果が表示されます。</li>
-          <li>自分のUser Codeは<a href="https://www.streetfighter.com/6/buckler/ja-jp" target="_blank" rel="noopener noreferrer">BUCKLER'S BOOT CAMP</a>で確認できます。</li>
+          <li>自分のUser Codeは<a href="https://www.streetfighter.com/6/buckler/ja-jp" target="_blank" rel="noopener noreferrer" className="App-link">BUCKLER'S BOOT CAMP</a>で確認できます。</li>
           <li>データはユーザーごとに保持しています。初めて実行するユーザーはデータの取得に少し時間がかかります。</li>
           <li>ユーザーのデータは初回の実行以降３時間ごとに取得されるようになります。最新の情報が反映されるまでには最大３時間かかります。</li>
           <li>対戦情報は過去100対戦分しか取得できないため初回実行後しばらくの間は１週間分の結果が見られない場合があります。最長１週間で見られるようになります。</li>

5a441230a96f282a5f8b5a79f775c0178ce1f392
Keiichi Tsuda <keiichi.tsuda@gmail.com>
2023-12-07 12:48:25 +0900

    refactor dump-replayreduced-as-csv.py

--- lambda/scripts/dump-replayreduced-as-csv.py
+++ lambda/scripts/dump-replayreduced-as-csv.py
@@ -1,10 +1,9 @@
 #!/opt/homebrew/bin/python3
 ##
-## Export battle log as csv (last 90 days)
+## Export battle log as csv
 ##
 import requests
 import csv
-import json
 import sys
 from datetime import datetime
 import pytz
@@ -18,61 +17,62 @@ config.read('dump-replayreduced-as-csv-config.ini')
 
 user_code = config['Settings']['user_code']
 days = config['Settings']['days']
+#days = 10 # overwrite settings
 aws_api_gateway_url = config['Settings']['aws_api_gateway_url']
 
 ##
-## Code
+## Functions
 ##
 def utc_to_jst(utc_timestamp):
-    utc_datetime = datetime.utcfromtimestamp(utc_timestamp)
-    utc_datetime = pytz.utc.localize(utc_datetime)
-    jst_datetime = utc_datetime.astimezone(pytz.timezone('Asia/Tokyo'))
-    return jst_datetime.strftime('%Y/%m/%d %H:%M:%S')
+  utc_datetime = datetime.utcfromtimestamp(utc_timestamp)
+  utc_datetime = pytz.utc.localize(utc_datetime)
+  jst_datetime = utc_datetime.astimezone(pytz.timezone('Asia/Tokyo'))
+  return jst_datetime.strftime('%Y/%m/%d %H:%M:%S')
 
-def json_to_csv(url):
-    # Send a GET request to the URL
-    response = requests.get(url)
-    response.raise_for_status()  # Check for errors
+def battle_log_to_csv(battle_log_list):
+  csv_list = []
+  for battle_log in battle_log_list:
+    replay_reduced = battle_log["ReplayReduced"]
+    opponent = replay_reduced["opponent"]
+    csv_list.append({
+      'UploadedAt': utc_to_jst((battle_log["UploadedAt"])),
+      'FighterID': replay_reduced["fighter_id"],
+      'ShortID': replay_reduced["short_id"],
+      'PlatformName': replay_reduced["platform_name"],
+      'CharacterName': replay_reduced["character_name"],
+      'LeaguePoint': replay_reduced["league_point"],
+      'BattleInputTypeName': replay_reduced["battle_input_type_name"],
+      'Result': replay_reduced["result"],
+      'RoundResults': "[" + ",".join(map(str, replay_reduced["round_results"])) + "]",
+      'OpponentFighterID': opponent["fighter_id"],
+      'OpponentShortID': opponent["short_id"],
+      'OpponentPlatformName': opponent["platform_name"],
+      'OpponentCharacterName': opponent["character_name"],
+      'OpponentLeaguePoint': opponent["league_point"],
+      'OpponentBattleInputTypeName': opponent["battle_input_type_name"],
+      'OpponentRoundResults': "[" + ",".join(map(str, opponent["round_results"])) + "]",
+      'Side': replay_reduced["side"],
+      'ReplayID': replay_reduced["replay_id"],
+      'Views': replay_reduced["views"],
+      'ReplayBattleTypeName': replay_reduced["replay_battle_type_name"]
+    })
+  return csv_list
 
-    # Load JSON data
-    json_data = response.json()
-
-    # Assume the JSON data is a list of dictionaries, if not, adjust accordingly
-    data_list = json_data
-
-    if not data_list:
-        return  # Return early if the list is empty
-
-    # Create a CSV writer object
-    writer = csv.writer(sys.stdout)
-
-    # Write the header
-    header = [
-        "UploadedAt", "FighterID", "ShortID", "PlatformName", "CharacterName", "LeaguePoint",
-        "BattleInputTypeName", "Result", "RoundResults", "OpponentFighterID", "OpponentShortID",
-        "OpponentPlatformName", "OpponentCharacterName", "OpponentLeaguePoint",
-        "OpponentBattleInputTypeName", "OpponentRoundResults", "Side",
-        "ReplayID", "Views", "ReplayBattleTypeName"
-    ]
-    writer.writerow(header)
-
-    # Write the data rows
-    for row in data_list:
-        replay_reduced = row["ReplayReduced"]
-        opponent = replay_reduced["opponent"]
-        uploaded_at = utc_to_jst((row["UploadedAt"]))
-        data_row = [
-            uploaded_at, replay_reduced["fighter_id"], replay_reduced["short_id"], replay_reduced["platform_name"],
-            replay_reduced["character_name"], replay_reduced["league_point"], replay_reduced["battle_input_type_name"],
-            replay_reduced["result"],
-            "[" + ",".join(map(str, replay_reduced["round_results"])) + "]", opponent["fighter_id"], opponent["short_id"],
-            opponent["platform_name"], opponent["character_name"], opponent["league_point"],
-            opponent["battle_input_type_name"], "[" + ",".join(map(str, opponent["round_results"])) + "]",
-            replay_reduced["side"], replay_reduced["replay_id"],
-            replay_reduced["views"], replay_reduced["replay_battle_type_name"]
-        ]
-        writer.writerow(data_row)
-
-# API Gateway URL
+##
+## main
+##
+# get battle log
 url = f'https://{aws_api_gateway_url}/retrieveBattleLog?USER_CODE={user_code}&RETRIEVE_OPTION={days}'
-json_to_csv(url)
+response = requests.get(url)
+response.raise_for_status()
+battle_log_list = response.json()
+
+# convert battle log to csv list
+csv_list = battle_log_to_csv(battle_log_list)
+sorted_csv_list = sorted(csv_list, key=lambda x: x['UploadedAt'], reverse=True)
+
+# print csv list to stdout
+fieldnames = sorted_csv_list[0].keys()
+writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+writer.writeheader()
+writer.writerows(sorted_csv_list)
